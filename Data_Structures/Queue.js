bc02902b7d79b07aaac4d623f7798610e2a08c9c
kmoonwright <kmoonwright@outlook.com>
2019-11-22 13:56:37 -0800

    Add Data Structure code and sample problems

--- Data_Structures/Queue.js
+++ Data_Structures/Queue.js
@@ -3,3 +3,55 @@
 // first-out(FIFO) principle. A queue's only two operations are enqueue (to insert 
 // an element onto the back of the queue), and dequeue (to remove the frontmost 
 // element).
+
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Queue { // FIFO
+    constructor() {
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.front = newNode;
+            this.back = newNode;
+            this.length += 1;
+        } else {
+            // let oldNode = this.back;
+            this.back.next = newNode
+            this.back = newNode;
+            // newNode.next = oldNode;
+            this.length += 1;
+        }
+        return this.length;
+    }
+
+    dequeue() {
+        let oldNode = this.front;
+        if (this.length === 0) return null;
+        if (this.length === 1) {
+            this.front = null;
+            this.back = null;
+            this.length -= 1;
+        } else {
+            oldNode = this.front;
+            let newNode = this.front.next;
+            this.front = newNode;
+            this.length -= 1;
+        }
+        return oldNode.value;
+    }
+
+    size() {
+        return this.length;
+    }
+}
--- Data_Structures/Stack.js
+++ Data_Structures/Stack.js
@@ -1,3 +1,55 @@
 // STACK - LIFO
 // A container of objects that are inserted and removed according to a last-in 
 // first-out(LIFO) principle. Elements can only be added and removed from the top.
+
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    // LIFO
+    constructor() {
+        this.bottom = null; // Front of the Stack
+        this.top = null; // Back of the stack
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (this.top) {
+            newNode.next = this.top;
+            this.top = newNode;
+            this.length += 1;
+        } else {
+            this.top = newNode;
+            this.bottom = newNode;
+            this.length += 1;
+        }
+        return this.length;
+    }
+
+    pop() {
+        let oldTop;
+        if (!this.top) return null;
+        if (this.length === 1) {
+            oldTop = this.top;
+            this.top = null;
+            this.bottom = null;
+            this.length -= 1;
+        } else {
+            oldTop = this.top;
+            let newTop = oldTop.next;
+            this.top = newTop;
+            this.length -= 1;
+        }
+        return oldTop.value;
+    }
+
+    size() {
+        return this.length
+    }
+}
