4c4f2e42dcb735d3bec669aa3fbfa9f8e074b21e
kmoonwright <kmoonwright@outlook.com>
2019-12-27 16:32:54 -0800

    Add basic js items

--- Rosetta Stone/JavaScript.js
+++ Rosetta Stone/JavaScript.js
@@ -1,8 +1,22 @@
 // VARIABLES
+var variable1 = 'a variable'
+let variable2 = 'a variable'
+const variable3 = 'a variable'
 
 // FUNCTIONS
+function example1(arg) {
+
+}
+
+let object = () => {}
 
 // CLASSES
+class banana {
+  constructor() {
+    
+  }
+}
+
 
 // OBJECTS
 
