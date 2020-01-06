8b62ec9495fb6facfb43170dae2dd73dff9dfe38
kmoonwright <kmoonwright@outlook.com>
2020-01-05 19:59:49 -0800

    Add typescript notes

--- Rosetta Stone/TypeScript.ts
+++ Rosetta Stone/TypeScript.ts
@@ -1,8 +1,54 @@
 
-// VARIABLE
+// Installation
+  // CLI
+  npm install - g typescript
 
-// FUNCTIONS(METHODS)
+// Compiling the code
+  //CLI
+  tsc fileName.ts
 
-// CLASSES
+  // Adding the type annotation
+function greeter(person: string) {
+  return "Hello, " + person;
+}
 
-// OBJECTS(HASHES)
+let user = "Jane User";
+
+document.body.textContent = greeter(user);
+
+
+// Interfaces (like objects)
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Jane", lastName: "User" };
+
+document.body.textContent = greeter(user);
+
+
+// Classes
+class Student {
+  fullName: string;
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.textContent = greeter(user);
