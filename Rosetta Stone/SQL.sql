9f0c1e31596604b407a61287274120962c4e243e
kmoonwright <kmoonwright@outlook.com>
2020-01-08 20:01:10 -0800

    Add SQL notes

--- Rosetta Stone/SQL.sql
+++ Rosetta Stone/SQL.sql
@@ -1,4 +1,17 @@
 -- GENERAL GUIDE
+-- Order of Operations
+  -- 1. FROM
+  -- 2. JOIN
+  -- 3. WHERE
+  -- 4. GROUP BY
+  -- 5. HAVING
+  -- 6. SELECT
+  -- 7. ORDER BY
+  -- 8. LIMIT/OFFSET
+
+
+-- EXAMPLE:
+SELECT
 
 
 -- SELECT
